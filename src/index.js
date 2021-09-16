4d20876110e3a0867560fd5218d40a13c9e368a0
unknown <cheastyakov3@gmail.com>
2021-09-16 23:45:28 +0300

    css-loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./styles/index.css"
+
 const obj = {
     foo: 'hey',
     bazz: 'hello'
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
     mode: "development",
+    plugins: [new MiniCssExtractPlugin()],
     module: {
         rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader,"css-loader"]
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -18,10 +25,14 @@ module.exports = {
     devtool: "source-map",
 
 
+
     devServer: {
         static: {
             directory: path.join(__dirname,"dist")
         },
-        compress: true
+        compress: true,
+        port: 8090,
+        hot: true,
+
     }
 }
